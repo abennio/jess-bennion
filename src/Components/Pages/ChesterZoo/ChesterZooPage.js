7824213628551111e38acdb553e1376df7535a7f
Alex <alex.bennion@icloud.com>
2024-09-24 13:10:42 +0100

    Updated Birkenhead & Chester Zoo pages and added new font

--- src/Components/Pages/ChesterZoo/ChesterZooPage.js
+++ src/Components/Pages/ChesterZoo/ChesterZooPage.js
@@ -13,7 +13,7 @@ function ChesterZooPage() {
     <>
       <div className="chester-zoo-page">
         <NavBar />
-        <div className="description">
+        <div className="chester-zoo-description">
           <h1>Chester Zoo</h1>
           <br></br>
           <p>
@@ -26,39 +26,55 @@ function ChesterZooPage() {
         </div>
       </div>
       <div className="zoo-image-container">
-        <div className="zoo-animation-and-desc">
-          <p>Animation</p>
-          <img
-            src={animation}
-            className="chester-zoo-animation"
-            alt="Chester Zoo Animation"
-          />
+        <div className="zoo-three-image-container">
+          <div className="zoo-animation-and-desc">
+            <h2>Animation</h2>
+            <img
+              src={animation}
+              className="chester-zoo-animation"
+              alt="Chester Zoo Animation"
+            />
+            <p>This animation was created to show on TV screens across the UK and on TVs across the Zoo.</p>
+            <hr></hr>
+          </div>
+          <div className="zoo-banner-and-desc">
+            <h2>Website Banner</h2>
+            <img
+              src={banner}
+              className="chester-zoo-banner"
+              alt="Chester Zoo Web Banner"
+            />
+            <p>As part of the brief set by Chester Zoo, a website banner was required so that they could
+               use it on their website as the hero banner on their home page.</p>
+            <hr></hr>
+          </div>
+          <div className="zoo-facebook-and-desc">
+            <h2>Facebook Ad</h2>
+            <img
+              src={facebook}
+              className="chester-zoo-facebook"
+              alt="Chester Zoo Facebook Advert"
+            />
+            <p>Chester Zoo's marketing plan included advertising the new attraction on Facebook. To make 
+              this stand out from other adverts on Facebook I decided to animate it. The shaking of the 
+              "ROOOAAAAARRRRRRR" text was added to catch the viewers eye, it needed to be short and
+              sweet to get the message across as succinctly as possible.</p>
+            <hr></hr>
+          </div>
         </div>
-        <div className="zoo-banner-and-desc">
-          <p>Website Banner</p>
-          <img
-            src={banner}
-            className="chester-zoo-banner"
-            alt="Chester Zoo Web Banner"
-          />
-        </div>
-        <div className="zoo-facebook-and-desc">
-          <p>Facebook Ad</p>
-          <img
-            src={facebook}
-            className="chester-zoo-facebook"
-            alt="Chester Zoo Facebook Advert"
-          />
-        </div>
-      </div>
         <div className="zoo-geofilter-and-desc">
-          <p>Snapchat Geofilter</p>
+          <h2>Snapchat Geofilter</h2>
           <img
             src={geofilter}
             className="chester-zoo-geofilter"
             alt="Chester Zoo Geofilter"
           />
+          <p>I decided to also create a Lion geofilter because this can be added to Snapchat for free and
+            allows Chester Zoo visitors to share Snapchat messages whilst they're viewing the new attraction.
+            This is a great idea to encourage visitors to spread the word without it looking like an advert.
+          </p>
         </div>
+      </div>
       <Footer />
     </>
   );
